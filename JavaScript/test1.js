08e8c8a4d20858fbca3d316cb9071190dde857cb
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-08 13:39:17 +0530

    Create test1.js

---
+++ JavaScript/test1.js
@@ -0,0 +1,17 @@
+<script>  
+var address=  
+{  
+company:"Javatpoint",  
+city:"Noida",  
+state:"UP",  
+fullAddress:function()  
+{  
+return this.company+" "+this.city+" "+this.state;  
+}  
+};  
+  
+  
+var fetch=address.fullAddress();  
+document.writeln(fetch);  
+  
+</script>  
