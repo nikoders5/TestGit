2daa324fa585e63df23742f5e93b6fe03f1a7f4b
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-08 14:19:00 +0530

    Update FileDemo.java

--- Java/FileDemo.java
+++ Java/FileDemo.java
@@ -1,6 +1,6 @@
 import java.io.*;  
-public class FileDemo {  
-    public static void main(String[] args) {  
+public FileDemo {  
+    static void (String[] args) {  
   
         try {  
             File file = new File("javaFile123.txt");  
