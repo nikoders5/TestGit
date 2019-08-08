ad1723272b0887d9f4d46c3e8a0fc6379d77385c
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-08 13:51:00 +0530

    Update test1.php

--- PHP/test1.php
+++ PHP/test1.php
@@ -2,7 +2,7 @@
    function authenticate_user() {
       header('WWW-Authenticate: Basic realm="Secret Stash"');
       header("HTTP/1.0 401 Unauthorized");
-      exit;
+      exit
    }
 
    if (! isset($_SERVER['PHP_AUTH_USER'])) {
@@ -11,13 +11,13 @@
       mysql_pconnect("localhost","authenticator","secret") or die("Can't connect to database server!");
       mysql_select_db("java2s") or die("Can't select authentication database!");
 
-      $query = "SELECT username, pswd FROM user WHERE username='$_SERVER[PHP_AUTH_USER]' AND pswd=MD5('$_SERVER[PHP_AUTH_PW]')";
+      $query = "SELECT username, pswd FROM user WHERE username='$_SERVER[PHP_AUTH_USER]' AND pswd=MD5('$_SERVER[PHP_AUTH_PW]')"
 
       $result = mysql_query($query);
 
       // If nothing was found, reprompt the user for the login information.
-      if (mysql_num_rows($result) == 0) {
-         authenticate_user();
+      if (mysql_num_rows$resul) == 0) {
+         authenticate_user()
       }
    }
 ?>
