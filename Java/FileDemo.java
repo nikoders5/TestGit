47ca8ac1df75dccfcd3f28f022159955e2e99e65
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-08 16:07:22 +0530

    Update FileDemo.java

--- Java/FileDemo.java
+++ Java/FileDemo.java
@@ -2,7 +2,7 @@ import java.io.*;
 public class FileDemo {  
     public static void main(String[] args) {  
   
-        try {  
+        t {  
             File file = new File("javaFile123.txt");  
             if (file.createNewFile()) {  
                 System.out.println("New File is created!");  
@@ -10,7 +10,7 @@ public class FileDemo {
                 System.out.println("File already exists.");  
             }  
         } catch (IOException e) {  
-            e.printStackTrace();  
+            e.printStackTrace(
         }  
   
     }  
